d36801ad11ff64c49ac7b49a742995135c02e2e8
Dmitry Sam <dmitry.sam88@gmail.com>
2017-12-25 17:18:48 +0200

    MVC 0.07

--- core/ImageManager.php
+++ core/ImageManager.php
@@ -19,7 +19,6 @@ class ImageManager{
     }
 
     public static function imageresize($outfile,$infile,$neww,$newh,$quality) {
-
         $im=imagecreatefromjpeg($infile);
         $im1=imagecreatetruecolor($neww,$newh);
         imagecopyresampled($im1,$im,0,0,0,0,$neww,$newh,imagesx($im),imagesy($im));
